6e748c6bd107e9ad45aba1398f0885ab8878aac1
sapphi-red <green@sapphi.red>
2020-07-06 01:16:54 +0900

    基本レイアウト

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
 <template>
-  <routes />
-  <router-view />
+  <div :class="$style.container">
+    <navigation :class="$style.navigation" />
+    <routes :class="$style.title" />
+    <main :class="$style.content">
+      <router-view />
+    </main>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Navigation from '/@/components/Navigation/Navigation.vue'
 import Routes from '/@/components/Routes.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
+    Navigation,
     Routes
   }
 })
 </script>
+
+<style lang="scss" module>
+.container {
+  display: grid;
+  grid-template-areas:
+    'nav title'
+    'nav content';
+}
+.navigation {
+  grid-area: nav;
+}
+.title {
+  grid-area: title;
+}
+.content {
+  grid-area: content;
+}
+</style>
---
+++ src/components/Navigation/Navigation.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav>
+    <logo-and-title />
+    <div>search</div>
+    <ul>
+      <li>Dashboard</li>
+      <li>Equipment List</li>
+      <li>Personal Property List</li>
+      <li>Register Item</li>
+    </ul>
+    <div>
+      <span>booQ Project {version}</span>
+      <div>about</div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import LogoAndTitle from './LogoAndTitle.vue'
+
+export default defineComponent({
+  name: 'Navigation',
+  components: {
+    LogoAndTitle
+  },
+  setup() {
+    return {}
+  }
+})
+</script>
