f6490d7eeb7d4f7e60d206996f54406b1cac6e25
sapphi-red <green@sapphi.red>
2020-07-06 09:17:23 +0900

    レイアウトの調整

--- src/components/Navigation/LogoAndTitle.vue
+++ src/components/Navigation/LogoAndTitle.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="$style.container">
-    <img src="/@/assets/img/logo-main.svg" />
+    <img :class="$style.logo" src="/@/assets/img/logo-main.svg" />
     <div>booQ</div>
   </div>
 </template>
@@ -19,5 +19,11 @@ export default defineComponent({
 <style lang="scss" module>
 .container {
   display: flex;
+  align-items: center;
+  font-size: 1.5rem;
+}
+.logo {
+  max-height: 1.5em;
+  max-width: 1.5em;
 }
 </style>
