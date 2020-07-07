90a25f23d7ae33245096d16f19407c578bb7491c
sapphi-red <green@sapphi.red>
2020-07-07 16:40:27 +0900

    remove webpackChunkname

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,63 +6,55 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'Dashboard',
-    component: () =>
-      import(/* webpackChunkname: "DashBoard" */ '/@/pages/DashBoard.vue')
+    component: () => import('/@/pages/DashBoard.vue')
   },
   {
     path: '/users',
     name: 'All Users',
-    component: () =>
-      import(/* webpackChunkname: "Users" */ '/@/pages/Users.vue')
+    component: () => import('/@/pages/Users.vue')
   },
   {
     path: '/users/:name',
     name: 'User Page',
-    component: () => import(/* webpackChunkname: "User" */ '/@/pages/User.vue')
+    component: () => import('/@/pages/User.vue')
   },
   {
     path: '/items',
     name: 'All Items',
-    component: () =>
-      import(/* webpackChunkname: "Items" */ '/@/pages/Items.vue'),
+    component: () => import('/@/pages/Items.vue'),
     props: { type: 'all' }
   },
   {
     path: '/items/new',
     name: 'Register Item',
-    component: () =>
-      import(/* webpackChunkname: "RegisterItem" */ '/@/pages/RegisterItem.vue')
+    component: () => import('/@/pages/RegisterItem.vue')
   },
   {
     path: '/items/equipment',
     name: 'Equipments',
-    component: () =>
-      import(/* webpackChunkname: "Item" */ '/@/pages/Items.vue'),
+    component: () => import('/@/pages/Items.vue'),
     props: { type: 'equipment' }
   },
   {
     path: '/items/property',
     name: 'Personal Property Items',
-    component: () =>
-      import(/* webpackChunkname: "Item" */ '/@/pages/Items.vue'),
+    component: () => import('/@/pages/Items.vue'),
     props: { type: 'property' }
   },
   {
     path: '/items/:id',
     name: 'Item',
-    component: () => import(/* webpackChunkname: "Item" */ '/@/pages/Item.vue')
+    component: () => import('/@/pages/Item.vue')
   },
   {
     path: '/admin',
     name: 'Admin Page',
-    component: () =>
-      import(/* webpackChunkname: "Admin" */ '/@/pages/Admin.vue')
+    component: () => import('/@/pages/Admin.vue')
   },
   {
     path: '/about',
     name: 'About',
-    component: () =>
-      import(/* webpackChunkname: "About" */ '/@/pages/About.vue')
+    component: () => import('/@/pages/About.vue')
   }
 ]
 
