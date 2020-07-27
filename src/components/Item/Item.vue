3cfbd26847cd21e4e2779b82aa396f8aa7a3af11
sapphi-red <green@sapphi.red>
2020-07-27 10:11:18 +0900

    ItemGrid

--- src/components/Item/Item.vue
+++ src/components/Item/Item.vue
@@ -1,25 +1,27 @@
 <template>
-  <div :class="$style.container">
-    <img :class="$style.img" :src="item.imgUrl" />
-    <div :class="$style.main">
-      <div>{{ item.name }}</div>
-      <div>{{ item.owners.map(owner => `@${owner.user.name}`).join() }}</div>
-    </div>
-    <div>
-      <div>
-        <icon name="thumbs-up" />{{
-          item.likeCounts > 0 ? item.likeCounts : ''
-        }}
+  <div :class="$style.wrapper" @click="onClick">
+    <img :class="$style.img" :src="imgUrl" />
+    <div :class="$style.container">
+      <div :class="$style.name">{{ item.name }}</div>
+      <div :class="$style.main">
+        <div :class="$style.likeCount">
+          <icon :class="$style.icon" name="thumbs-up" />{{ likeCount }}
+        </div>
+        <div :class="$style.owners">
+          {{ item.owners.map(owner => `@${owner.user.name}`).join() }}
+        </div>
+        <slot name="controls" />
       </div>
-      <slot name="controls" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { defineComponent, PropType, computed } from 'vue'
+import { useRouter } from 'vue-router'
 import { ItemSummary } from '/@/lib/apis'
 import Icon from '/@/components/UI/Icon.vue'
+import NoImg from '/@/assets/img/no-image.svg'
 
 export default defineComponent({
   name: 'Item',
@@ -32,24 +34,72 @@ export default defineComponent({
       required: true
     }
   },
-  setup() {
-    return {}
+  setup(props) {
+    const router = useRouter()
+
+    const imgUrl = computed(() =>
+      props.item.imgUrl ? props.item.imgUrl : NoImg
+    )
+    const likeCount = computed(() =>
+      props.item.likeCounts > 0 ? props.item.likeCounts : ''
+    )
+    const onClick = () => {
+      router.push(`/items/${props.item.id}`)
+    }
+
+    return { imgUrl, likeCount, onClick }
   }
 })
 </script>
 
 <style lang="scss" module>
+.wrapper {
+  position: relative;
+  width: 100%;
+  &::before {
+    content: '';
+    display: block;
+    // B6判
+    padding-top: (182 / 128) * 100%;
+  }
+  cursor: pointer;
+  &:hover {
+    filter: opacity(0.8);
+  }
+}
 .container {
-  display: flex;
-  align-items: center;
-  padding: 16px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: calc(100% - 16px);
+  padding: 8px;
+  background: rgba(255, 255, 255, 0.5);
+  backdrop-filter: blur(2px);
+  word-break: break-all;
 }
 .img {
-  height: 120px;
-  width: 80px;
-  object-fit: contain;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
 }
 .main {
-  flex: 1;
+  display: flex;
+  align-items: center;
+}
+.name {
+  font-weight: bold;
+}
+.likeCount {
+  min-width: max-content;
+  margin-right: 4px;
+}
+.owners {
+  margin-left: auto;
+}
+.icon {
+  vertical-align: middle;
 }
 </style>
---
+++ src/components/Item/Items.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul :class="$style.list">
+    <li v-for="item in items" :key="item.id">
+      <item :item="item" />
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { ItemSummary } from '/@/lib/apis'
+import Item from './Item.vue'
+
+export default defineComponent({
+  name: 'Items',
+  components: {
+    Item
+  },
+  props: {
+    items: {
+      type: Array as PropType<ItemSummary[]>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style lang="scss" module>
+.list {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(14rem, 1fr));
+  gap: 16px;
+}
+</style>
