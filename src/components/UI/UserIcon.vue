585802dacc27e1ca257e9727e48c31b28ffaf9dd
ryoha <the7uya@gmail.com>
2020-08-15 18:28:42 +0900

    add UI/UserIcon

---
+++ src/components/UI/UserIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <img :src="iconUri" :width="size" :height="size" />
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+
+export default defineComponent({
+  name: 'UserIcon',
+  props: {
+    userName: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: Number,
+      default: 36
+    }
+  },
+  setup(props) {
+    const iconUri = computed(
+      () => `https://q.trap.jp/api/v3/public/icon/${props.userName}`
+    )
+    return { iconUri }
+  }
+})
+</script>
