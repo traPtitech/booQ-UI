c215d242e5ce1176367b78595dbbde1559d4d37f
ryoha <the7uya@gmail.com>
2020-08-16 08:49:05 +0900

    add Comments

---
+++ src/components/ItemDetail/Comments.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h3>コメント</h3>
+    <div :class="$style.itemContainer">
+      <div v-for="(comment, i) in comments" :key="i" :class="$style.item">
+        <user-icon :user-name="comment.user.name" />
+        <div :class="$style.text">{{ comment.text }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { Comment } from '/@/lib/apis'
+import UserIcon from '/@/components/UI/UserIcon.vue'
+
+export default defineComponent({
+  name: 'Comments',
+  components: { UserIcon },
+  props: {
+    comments: {
+      type: Array as PropType<Comment[]>,
+      default: []
+    }
+  },
+  setup() {
+    return
+  }
+})
+</script>
+
+<style lang="scss" module>
+.itemContainer {
+  padding-left: 8px;
+}
+
+.item {
+  display: flex;
+}
+
+.text {
+  margin-left: 16px;
+  align-self: center;
+}
+</style>
