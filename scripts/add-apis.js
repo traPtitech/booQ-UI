59f9c22f2d2a9566c0f3c315d30b5b0d0ade1422
sapphi-red <green@sapphi.red>
2020-09-08 14:58:32 +0900

    Update openapi-generator to 5.0.0-beta2

--- scripts/add-apis.js
+++ scripts/add-apis.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
-const { Project, ts } = require('ts-morph')
+const { Project } = require('ts-morph')
 
 const generateBaseAPI = async sourceFile => {
   const classes = sourceFile.getClasses()
@@ -29,55 +29,13 @@ const generateBaseAPI = async sourceFile => {
   await sourceFile.save()
 }
 
-const patchUrl = async sourceFile => {
-  const imports = sourceFile.getImportDeclarations()
-
-  const im = imports.find(im => im.getModuleSpecifierValue() === 'url')
-  im.setModuleSpecifier('/@/patch/url')
-
-  await sourceFile.save()
-}
-
-const ignoreErrorWithDelete = async sourceFile => {
-  const creatorFuncs = sourceFile
-    .getVariableDeclarations()
-    .filter(d => d.getName().endsWith('Creator'))
-
-  const returns = creatorFuncs
-    .map(d => d.getInitializerIfKindOrThrow(ts.SyntaxKind.FunctionExpression))
-    .flatMap(f => f.getBody().getChildrenOfKind(ts.SyntaxKind.ReturnStatement))
-
-  const properties = returns
-    .map(r => r.getExpressionOrThrow())
-    .filter(e => e.getKind() === ts.SyntaxKind.ObjectLiteralExpression)
-    .flatMap(o => o.getProperties())
-    .filter(p => p.getKind() === ts.SyntaxKind.PropertyAssignment)
-
-  const deletes = properties
-    .map(p => p.getInitializerIfKindOrThrow(ts.SyntaxKind.ArrowFunction))
-    .flatMap(i =>
-      i.getBody().getChildrenOfKind(ts.SyntaxKind.ExpressionStatement)
-    )
-    .flatMap(es => es.getChildrenOfKind(ts.SyntaxKind.DeleteExpression))
-
-  deletes.forEach(d => {
-    const i = d.getChildIndex()
-    const parent = d.getParentIfKindOrThrow(ts.SyntaxKind.ExpressionStatement)
-    parent.insertJsDoc(i, '@ts-ignore')
-  })
-
-  await sourceFile.save()
-}
-
 const addApis = async dir => {
   const project = new Project()
   project.addSourceFilesAtPaths(`${dir}/**/*.ts`)
 
   const sourceFile = project.getSourceFileOrThrow('api.ts')
 
-  await ignoreErrorWithDelete(sourceFile)
   await generateBaseAPI(sourceFile)
-  await patchUrl(sourceFile)
 }
 
 module.exports = addApis
