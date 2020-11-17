866625a952d20e6a4fb9d549ea019ad032885e25
sapphi-red <green@sapphi.red>
2020-11-17 10:48:16 +0900

    z-index

--- src/components/UI/SearchInput.vue
+++ src/components/UI/SearchInput.vue
@@ -86,11 +86,13 @@ export default defineComponent({
 .underline {
   background-color: $color-text-secondary;
   left: 0;
+  z-index: 1;
 }
 .underlineFocus {
   background-color: $color-primary;
   left: -100%;
   transition: all 0.2s ease;
+  z-index: 2;
   .container:focus-within & {
     left: 0;
   }
