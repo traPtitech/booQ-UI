ad2903ab16a761e5cfb76a90c151a415254b02e2
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-12-17 00:08:20 +0900

    add LikeButton

--- src/components/ItemDetail/Information.vue
+++ src/components/ItemDetail/Information.vue
@@ -2,7 +2,11 @@
   <div :class="$style.container">
     <div :class="$style.titleWrapper">
       <h2>{{ item.name }}</h2>
-      <button :class="$style.favBtn">💛</button>
+      <like-button
+        :class="$style.favBtn"
+        :likes="item.likes"
+        :item-id="item.id"
+      />
     </div>
     <div>{{ item.description }}</div>
     <owners :item="item" />
@@ -13,21 +17,23 @@
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
-import { ItemSummary } from '/@/lib/apis'
+import { ItemDetail } from '/@/lib/apis'
 import Owners from './Owners.vue'
 import Comments from './Comments.vue'
 import Logs from './Logs.vue'
+import LikeButton from './LikeButton.vue'
 
 export default defineComponent({
   name: 'Information',
   components: {
     Owners,
     Comments,
-    Logs
+    Logs,
+    LikeButton
   },
   props: {
     item: {
-      type: Object as PropType<ItemSummary>,
+      type: Object as PropType<ItemDetail>,
       required: true
     }
   },
---
+++ src/components/ItemDetail/LikeButton.vue
@@ -0,0 +1,80 @@
+<template>
+  <button :class="$style.container" @click="toggleLike">
+    <icon v-if="!isLiked" name="mdi:heart-outline" :size="32" />
+    <icon v-else name="mdi:heart" :size="32" :class="$style.liked" />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, PropType } from 'vue'
+import IconButton from '/@/components/UI/IconButton.vue'
+import Icon from '/@/components/UI/Icon.vue'
+import apis, { User } from '/@/lib/apis'
+import useMe from '/@/use/me'
+import { useStore } from '/@/store'
+
+export default defineComponent({
+  name: 'LikeButton',
+  components: { Icon },
+  props: {
+    itemId: {
+      type: Number,
+      required: true
+    },
+    likes: {
+      type: Array as PropType<User[]>,
+      default: []
+    }
+  },
+  setup(props) {
+    const { id: meID } = useMe()
+    const isLiked = ref(props.likes.findIndex(v => meID.value === v.id) > -1)
+    const toggleLike = async () => {
+      try {
+        await apis.addLike(props.itemId)
+        isLiked.value = !isLiked.value
+      } catch (e) {
+        const store = useStore()
+        store.commit.addToast({
+          type: 'error',
+          text: '「いいね」に失敗しました'
+        })
+      }
+    }
+    return { isLiked, toggleLike }
+  }
+})
+</script>
+
+<style lang="scss" module>
+.container {
+  cursor: pointer;
+
+  &:focus {
+    outline: 0;
+  }
+}
+.liked {
+  animation: clicked 0.5s;
+  color: red;
+}
+
+@keyframes clicked {
+  0% {
+    color: transparent;
+  }
+  25% {
+    transform: translateY(-0.5rem);
+    color: red;
+  }
+  50% {
+    transform: translateY(0);
+  }
+  75% {
+    transform: translateY(-0.25rem);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+</style>
