2188de24297d25d22a046afb9b2017d1b68ce0b3
sapphi-red <green@sapphi.red>
2021-01-08 00:14:26 +0900

    Fix for vite@2

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,38 @@
 import { UserConfig } from 'vite'
+//@ts-expect-error: to avoid installing @types/node
 import path from 'path'
 import packageJson from './package.json'
+import VuePlugin from '@vitejs/plugin-vue'
 import PurgeIcons from 'vite-plugin-purge-icons'
+
+declare const __dirname: string
 
 const srcPath = path.resolve(__dirname, 'src').replace(/\\/g, '/')
 
 const config: UserConfig = {
   alias: {
-    '/@/': srcPath
+    '/@': srcPath
   },
-  proxy: {
-    '/api': {
-      target: 'https://booq-dev.trapti.tech',
-      changeOrigin: true,
-      ws: false
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://booq-dev.trapti.tech',
+        changeOrigin: true,
+        ws: false
+      }
     }
   },
-  cssPreprocessOptions: {
-    scss: {
-      additionalData: `@import "${srcPath}/styles/common.scss";`
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "${srcPath}/styles/common.scss";`
+      }
     }
   },
   define: {
     __VERSION__: packageJson.version
   },
-  plugins: [PurgeIcons()]
+  plugins: [VuePlugin(), PurgeIcons()]
 }
 
 export default config
