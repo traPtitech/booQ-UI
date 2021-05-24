ae6e76f21afbadaf8382de526d6b6b8cd8105a0f
ryoha000 <the7uya@gmail.com>
2021-05-25 04:10:44 +0900

    fix useCart

--- src/components/Item/use/cart.ts
+++ src/components/Item/use/cart.ts
@@ -5,7 +5,7 @@ import useOpener from '/@/use/opener'
 
 const useCart = (props: { items: ItemSummary[] }) => {
 	const store = useStore()
-	const itemStatus = computed(() => props.items.map(item => !!store.state.itemInCart.find(iic => iic.id === item.id )))
+	const cartCounts = computed(() => props.items.map(item => store.state.itemInCart.find(iic => iic.id === item.id )?.count ?? 0))
 
 	const addItemToCart = (payload: { id: number, count: number }) => {
 		store.commit.addItemToCart(payload)
@@ -20,13 +20,13 @@ const useCart = (props: { items: ItemSummary[] }) => {
 	const clickAddDialog = (i: number) => {
 		addDialogItem.value = props.items[i] || null
 		if (!addDialogItem.value) return
-		if (itemStatus.value[i]) {
+		if (cartCounts.value[i]) {
 			removeItemFromCart(addDialogItem.value.id)
 		} else {
 			toggleAddDialog()
 		}
 	}
-	return { itemStatus, addItemToCart, removeItemFromCart, isOpenAddDialog, toggleAddDialog, clickAddDialog, addDialogItem }
+	return { cartCounts, addItemToCart, removeItemFromCart, isOpenAddDialog, toggleAddDialog, clickAddDialog, addDialogItem }
 }
 
 export default useCart
