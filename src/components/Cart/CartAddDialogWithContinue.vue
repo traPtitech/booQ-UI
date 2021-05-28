400d729a09231ce3a6b69466358f0c56c5a97f6d
ryoha000 <the7uya@gmail.com>
2021-05-29 04:01:02 +0900

    fix CartAddDialogWithContinue

--- src/components/Cart/CartAddDialogWithContinue.vue
+++ src/components/Cart/CartAddDialogWithContinue.vue
@@ -1,35 +1,68 @@
 <template>
-  <cart-add-dialog :item="item" :btn="btn" @close="close" @submit="openConfirm">
-    <template #continue>
-      <div :class="$style.continue">
+  <dialog-template :title="title" @close="close">
+    <div :class="$style.container">
+      <form>
+        <owner-selector
+          v-if="item.type !== ItemType.equipment"
+          v-model="ownerName"
+          :owner-details="ownerDetails"
+          :class="$style.input"
+        />
+        <input-number
+          v-model="count"
+          :class="$style.input"
+          label="個数"
+          :max="maxCount"
+          :min="isEdit ? 0 : 1"
+        />
+        <wide-icon-button
+          v-if="maxCount === 0"
+          icon="mdi:cancel"
+          label="在庫がありません"
+          variant="caution"
+          :class="$style.button"
+          :disabled="true"
+        />
+        <wide-icon-button
+          v-else
+          icon="mdi:arrow-right-bold-circle"
+          label="次にすすむ"
+          :class="$style.button"
+          @click="goNext"
+        />
         <div :class="$style.or">または</div>
         <wide-icon-button
           icon="mdi:cart"
           label="まだ借りる"
           type="submit"
           :class="$style.button"
-          @click="jumpPage"
+          @click="goOn"
         />
         <div :class="$style.description">
           「まだ借りる」でまとめて目的や返却日を入力できます
         </div>
-      </div>
-    </template>
-  </cart-add-dialog>
+      </form>
+    </div>
+  </dialog-template>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
+import DialogTemplate from '/@/components/UI/DialogTemplate.vue'
 import WideIconButton from '/@/components/UI/WideIconButton.vue'
-import CartAddDialog from './CartAddDialog.vue'
-import { ItemSummary } from '/@/lib/apis'
+import OwnerSelector from '/@/components/ItemDetail/OwnerSelector.vue'
+import InputNumber from '/@/components/UI/InputNumber.vue'
+import { ItemSummary, ItemType } from '/@/lib/apis'
+import useAddCart from './use/addCart'
 import { useRouter } from 'vue-router'
 
 export default defineComponent({
   name: 'CartAddDialogWithContinue',
   components: {
-    CartAddDialog,
-    WideIconButton
+    DialogTemplate,
+    WideIconButton,
+    InputNumber,
+    OwnerSelector
   },
   props: {
     item: {
@@ -40,40 +73,50 @@ export default defineComponent({
   emits: {
     close: () => true
   },
-  setup(_, context) {
-    const close = () => {
-      context.emit('close')
-    }
-
-    // submitよりbuttonのonclickのほうがはやく発火するため記録しておく
-    let isJump = false
-    const jumpPage = () => {
-      isJump = true
-    }
+  setup(props, { emit }) {
+    const {
+      title,
+      count,
+      isEdit,
+      submit,
+      close,
+      ownerName,
+      ownerDetails,
+      maxCount
+    } = useAddCart(props, emit)
 
     const router = useRouter()
-    const openConfirm = () => {
-      if (isJump) {
-        router.push('/items/equipment')
-        isJump = false
-      } else {
-        router.push('/cart')
-        isJump = false
-      }
+    const goNext = () => {
+      submit()
+      router.push('/cart')
     }
-
-    const btn = { icon: 'mdi:arrow-right-bold-circle', label: '次にすすむ' }
+    const goOn = () => {
+      submit()
+    }
     return {
+      title,
+      count,
       close,
-      jumpPage,
-      openConfirm,
-      btn
+      isEdit,
+      ownerName,
+      ownerDetails,
+      maxCount,
+      ItemType,
+      goOn,
+      goNext
     }
   }
 })
 </script>
 
 <style lang="scss" module>
+.container {
+  max-width: 360px;
+}
+.input {
+  margin-bottom: 2rem;
+  width: 100%;
+}
 .button {
   width: 100%;
   margin: auto;
