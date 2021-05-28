207eaae4dfe8bfed936969955b3b0603e358b6f4
ryoha000 <the7uya@gmail.com>
2021-05-29 03:59:29 +0900

    add EditingSubmitButton

---
+++ src/components/Cart/EditingSubmitButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <wide-icon-button
+    v-if="count !== 0"
+    icon="mdi:arrow-right-bold-circle"
+    label="変更する"
+    :class="$style.button"
+  />
+  <wide-icon-button
+    v-else
+    icon="mdi:arrow-right-bold-circle"
+    label="削除する"
+    variant="caution"
+    :class="$style.button"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import WideIconButton from '/@/components/UI/WideIconButton.vue'
+
+export default defineComponent({
+  name: 'EditingSubmitButton',
+  components: {
+    WideIconButton
+  },
+  props: {
+    count: {
+      type: Number,
+      required: true
+    }
+  },
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" module>
+.button {
+  width: 100%;
+  margin: auto;
+}
+</style>
