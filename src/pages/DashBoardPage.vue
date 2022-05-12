4c4e159e8853ed9152f4cc48703d62993f9d9731
s9 <63638257+s9tack@users.noreply.github.com>
2022-05-12 20:58:30 +0900

    h3をpaddingに変更

--- src/pages/DashBoardPage.vue
+++ src/pages/DashBoardPage.vue
@@ -10,9 +10,7 @@
       />
     </ul>
   </div>
-  <div v-else>
-    <h3>借りてるアイテムはありません</h3>
-  </div>
+  <div v-else :class="$style.noitem">借りてるアイテムはありません</div>
 </template>
 
 <script lang="ts" setup>
@@ -47,5 +45,8 @@ onMounted(async () => {
   grid-template-columns: repeat(auto-fill, minmax(min(25rem, 100%), 1fr));
   gap: 1rem;
   margin: 1rem;
+}
+.noitem {
+  padding: 3rem;
 }
 </style>
